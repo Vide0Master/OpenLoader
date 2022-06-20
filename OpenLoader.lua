f9f6ef2d9663b7fd9a65a490af899cb452ef8fac
Vide0Master <36771576+Vide0Master@users.noreply.github.com>
2022-06-20 12:34:13 +0300

    Update OpenLoader.lua

--- OpenLoader.lua
+++ OpenLoader.lua
@@ -1,8 +1,27 @@
 local gpu = component.proxy(component.list("gpu")())
-local fss = component.list("filesystem")
+local fss={}
 
+settings={
+    boot_file="init.lua"
+}
+
+for k,v in component.list("filesystem") do
+    fss[#fss+1]=k
+end
 while true do
     for i=1,#fss do
-        gpu.set(1,i,i.."."..fss[i])
+        local fs=component.proxy(fss[i])
+        local files = fs.list("\\")
+        for k,v in files do
+            local b_file=""
+            if v==settings.boot_file then 
+                local file = fs.open(v, 'r')
+                while true do 
+                    local smb=fs.read(file,1)
+                    if smb==nil then break else b_file=b_file+smb end
+                end
+                local exe, err=load(file)
+            end
+        end
     end
 end
